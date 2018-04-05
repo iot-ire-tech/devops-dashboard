96847081725b07120518a7a4041c95980686b38f
Anthony Ennis <tonyennis@yahoo.com>
2018-04-05 12:01:16 +0100

    Update queryIt.js

--- queryIt.js
+++ queryIt.js
@@ -1,5 +1,18 @@
+var urlRest="http://example.com/"
+
 $(document).ready(function () {
-	alert("hi")
+	var endPointsArr=["/"];
+	
+	endPointsArr.forEach(function (ep) {
+
+		url = urlRest + "/";
+		var tmpObj = nw.setUrl(url).setMessage("Retrieving Status Information").getMe().getResult();
+	//	nw.setUrl(url).setMethod("post").setPayload(dataIn).setMessage("Posting New Authentication").sendMe().getStatus()
+		// Update cell status
+		$(".status").html("Good");
+		
+	});
+	
 });
 
 
